7522aefb9dfc79c1332abb29711cc0225490a238
lavinas-science <lavinas@gmail.com>
2021-09-21 21:01:41 -0300

    elastic 2

--- clients/elasticsearch/es_client.go
+++ clients/elasticsearch/es_client.go
@@ -6,10 +6,9 @@ import (
 	"time"
 	"os"
 	"net/http"
-
 )
 
-func getClient() {
+func GetClient() {
 	client, err := elastic.NewClient(
 		elastic.SetURL("http://127.0.0.1:9200"),
 		elastic.SetHealthcheckInterval(10*time.Second),
